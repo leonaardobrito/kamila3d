48b6a38db2997357955424e4df4491674b3b0a68
Leonardo <leonardonunesbrito@gmail.com>
2024-04-11 22:13:14 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, ScrollControls } from "@react-three/drei";
+import Scene from "../public/Scene";
+
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Canvas style={{ width: '100vw', height: '100vh' }}>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 10, 5]} intensity={0.3} />
+        <OrbitControls
+          enableZoom={false}
+          enablePan={false}
+          enableRotate={true}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+          rotateSpeed={0.435}
+        />
+        <ScrollControls pages={3}>
+          <Scene />
+        </ScrollControls>
+      </Canvas>
+    </div>
+  );
+}
+
+export default App;
